f2914375841b5a967199e0b557b62b989beacd16
Liz Borchardt <zardra@gmail.com>
2014-02-19 12:49:36 -0800

    Django app tutorial, part3, exceptions and namespaces

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 # from django.template import RequestContext, loader
 from polls.models import Poll
 
@@ -17,8 +17,14 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
-
+    # return HttpResponse("You're looking at poll %s." % poll_id)
+    # try:
+    #     poll = Poll.objects.get(pk=poll_id)
+    # except Poll.DoesNotExist:
+    #     raise Http404
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
+    
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
 
